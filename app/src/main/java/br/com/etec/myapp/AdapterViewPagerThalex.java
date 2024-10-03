2eec501feab6bbb2986d695baf83bd884b669b23
thalitacorreia <thalitacorreia398@gmail.com>
2024-10-03 19:57:22 -0300

    View Pager

---
+++ app/src/main/java/br/com/etec/myapp/AdapterViewPagerThalex.java
@@ -0,0 +1,32 @@
+package br.com.etec.myapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class AdapterViewPagerThalex extends FragmentStateAdapter {
+    public AdapterViewPagerThalex(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new ChaveirosFragment();
+            case 1:
+                return new PulseirasFragment();
+            case 2:
+                return new ColaresFragment();
+            case 3:
+                return new AdesivosFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
