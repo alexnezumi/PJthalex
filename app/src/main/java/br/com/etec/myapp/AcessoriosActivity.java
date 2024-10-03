db487e8841d2fd9909cc1a67e905ff14754e6340
thalitacorreia <thalitacorreia398@gmail.com>
2024-10-03 20:24:41 -0300

    Tab Implement smt

--- app/src/main/java/br/com/etec/myapp/AcessoriosActivity.java
+++ app/src/main/java/br/com/etec/myapp/AcessoriosActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class AcessoriosActivity extends AppCompatActivity {
+    TabLayout idTabAcessorio;
+    ViewPager2 idViewPagerAcessorios;
+    AdapterViewPagerThalex adapterViewPagerThalex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,38 @@ public class AcessoriosActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        idTabAcessorio = findViewById(R.id.idTabAcessorios);
+        idViewPagerAcessorios = findViewById(R.id.idViewPagerAcessorios);
+
+        adapterViewPagerThalex = new AdapterViewPagerThalex(this);
+
+        idViewPagerAcessorios.setAdapter(adapterViewPagerThalex);
+
+        idTabAcessorio.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                idViewPagerAcessorios.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        idViewPagerAcessorios.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                idTabAcessorio.getTabAt(position);
+            }
         });
     }
 }
